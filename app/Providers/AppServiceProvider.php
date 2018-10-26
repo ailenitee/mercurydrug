6cb39c02f79a76ecb422352cc7bec639d3534775
Aileni <aileni.tee@glyphgames.com>
2018-10-26 17:01:00 +0800

    pw resets delete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
   */
   public function boot()
   {
+    Schema::defaultStringLength(191);
     if(!session('cart'))
     {
       session(['cart' => new EasyCart()]);
